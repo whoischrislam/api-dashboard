9459ce01b24258c7b6dfa7aaab4f383880766c50
whoischrislam <whoischrislam@gmail.com>
2024-03-22 17:16:10 -1000

    finish

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-/**
- * Challenge: get a random image from Unsplash and set it as the background
- * 
- * Part 1:
- * 
- * URL: https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature
- * (You can change the "query" at the end to whatever theme you want)
- * 
- * Change the body's backgroundImage to: 
- * `url(<insert the URL of the iamge from the API here>)`
- *  d
- * (You may need to dig around the response body a bit to find this URL)
- * 
- * (Note I've already added some CSS to resize the image within the window.
- * Instructions for this were found on CSS Tricks: 
- * https://css-tricks.com/perfect-full-page-background-image/#awesome-easy-progressive-css3-way)
- */
 let backgroundImageURL = "";
 
 async function getImage() {
